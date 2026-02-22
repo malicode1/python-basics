71c263178058ebc51b9bcc37e19003d3c8ec8757
malicode1 <mehmetalimuftuoglu19610@gmail.com>
2026-02-22 16:47:55 +0300

    Add input and sum example

--- basics.py
+++ basics.py
@@ -1,1 +1,10 @@
 print("Hello Github")
+
+name = input("Enter your name: ")
+print("Hello " +  name)
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+result = num_1 + num_2
+print("sum:",result)
