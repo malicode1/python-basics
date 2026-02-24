0bf4e4cb586bca56ef775caaa3ee4c779757bf6e
malicode1 <mehmetalimuftuoglu19610@gmail.com>
2026-02-24 18:43:50 +0300

    Added input practice and gitignore

---
+++ 01_input_output.py
@@ -0,0 +1,15 @@
+print("--User greeting---")
+
+name = input("Enter your name: ")
+print("Hello,", name)
+
+print("\n---Math Section---")
+
+num_1 = float(input("Enter first number: "))
+num_2 = float(input("Enter second number: "))
+
+sum_result = num_1 + num_2
+multiply_result = num_1 * num_2
+
+print("Sum:",sum_result)
+print("Multiplication:",multiply_result)
