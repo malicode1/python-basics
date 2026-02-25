3428fe561e21a5b5cbaa1e39ac58efe3cd912e6f
malicode1 <mehmetalimuftuoglu19610@gmail.com>
2026-02-25 20:57:53 +0300

    Added calculator with conditions

---
+++ 02_conditions.py
@@ -0,0 +1,22 @@
+print("---CONDITIONS---")
+
+print("\n---CALCULATOR---")
+
+num_1 = float(input("Enter first number: "))
+operator = input("Enter operator (+, -, *, /): ")
+num_2 = float(input("Enter second number: "))
+
+if operator == "+":
+    result = num_1 + num_2
+elif operator == "-":
+    result = num_1 - num_2
+elif operator == "*":
+    result = num_1 * num_2
+elif operator == "/":
+    if num_2 == 0:
+        result = "Cannot divide by zero"
+    else:
+        result = num_1 / num_2
+else:
+    result = "Invalid operator!"
+print("Result:",result)
