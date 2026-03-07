c7c32f894a61ecb632cb318175bbfc6399d4cd56
malicode1 <mehmetalimuftuoglu19610@gmail.com>
2026-03-07 14:37:36 +0300

    added mad libs mini project

---
+++ projects/mini_projects/mad_libs.py
@@ -0,0 +1,13 @@
+# project Mad Libs Game
+
+print("---Mad Libs Game---")
+
+adjective = input("Enter adjective: ")
+noun = input("Enter noun: ")
+verb = input("Enter verb: ")
+place = input("Enter place: ")
+
+story = f"Today I went to the {place}. It was very {adjective}. I saw a {noun} that could {verb}."
+
+print("\nHere is your story:")
+print(story)
